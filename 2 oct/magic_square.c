1ef5f8bc984e986c6a98f922579b642595ffbf8f
Manaswini Madarapu <103044632+Manaswini-Madarapu@users.noreply.github.com>
2023-10-03 09:17:30 +0530

    Add files via upload

---
+++ 2 oct/magic_square.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+int fun(int n)
+{
+	int even[n],odd[n];
+	int i,ke=0,ko=0;
+	for(i=1;i<=n*n;i++)
+	{
+		if (i%2==0)
+		{
+			even[ke]=1;
+			ke++;
+		}
+		else
+		{
+			odd[ko]=i;
+			ko++;
+		}
+	}
+	return even[],odd[];
+}
+void main()
+{
+	int i,j,ke=0,ko=0,n=3;
+	even[],odd[]=fun(n)
+	for(i=1;i<=n;i++)
+	{
+		for(j=1;j<=n,j++)
+		{
+			if(i-1 ==2 && j==2)
+			{
+				arr[i][j]=5;
+			}
+			else if((i+j)%2==0)
+			{
+				
+			}
+		}
+	}
+}
